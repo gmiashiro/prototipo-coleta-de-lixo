9efe987d2b6bce12a749374b4a7624ffdbda08ec
vitoria-la <laura.v.fabiani@gmail.com>
2025-06-16 10:56:43 -0300

    Continuando a modificar o CSS

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!tabela) return;
   
     const dados = [
-      { dia: "Segunda-feira", bairro: "Zona 1, Zona 2", tipo: "Orgânico" },
-      { dia: "Terça-feira", bairro: "Zona 3, Zona 4", tipo: "Reciclável" },
-      { dia: "Quarta-feira", bairro: "Zona 5, Zona 6", tipo: "Orgânico" },
-      { dia: "Quinta-feira", bairro: "Zona 7, Zona 8", tipo: "Reciclável" },
-      { dia: "Sexta-feira", bairro: "Zona 9, Zona 10", tipo: "Orgânico" },
+      { bairro: "Zona 1", periodo: "Manhã" },
+      {bairro: "Zona 3", periodo: "Manhã" },
+      {bairro: "Zona 5", periodo: "Manhã" },
+      {bairro: "Zona 7", periodo: "Manhã" },
+      {bairro: "Zona 9", periodo: "Manhã" },
     ];
   
     dados.forEach(item => {
       const row = document.createElement("tr");
-      row.innerHTML = `<td>${item.dia}</td><td>${item.bairro}</td><td>${item.tipo}</td>`;
+      row.innerHTML = `<td>${item.bairro}</td><td>${item.periodo}</td>`;
       tabela.appendChild(row);
     });
   });
